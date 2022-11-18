9236d69db22cda7129cb3bcaba21a6d46e3e1067
Vadim <v4dim-p0pov@yandex.ru>
2022-11-18 10:18:05 +0700

    first

---
+++ Practica/AddEditPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practica
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+       private Client _currentClient = new Client();
+
+        public AddEditPage(Client SelectClient)
+        {
+            InitializeComponent();
+            if (SelectClient != null) 
+            {
+                _currentClient = SelectClient;
+            }
+        
+            DataContext = _currentClient;
+            Combogender.ItemsSource = practicaEntities.GetContext().Gender.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrEmpty(_currentClient.FirstName))
+               
+                errors.AppendLine("Укажите Имя");
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentClient.ID == 0)
+                practicaEntities.GetContext().Client.Add(_currentClient);
+
+            try
+            {
+                practicaEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                NavigationService.Navigate(new ClientView());
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+    }
+    }
+}
---
+++ Practica/ClientView.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practica
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientView.xaml
+    /// </summary>
+    public partial class ClientView : Page
+    {
+        public ClientView()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void BtnBackToMain_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new HelloPage());
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddEditPage((sender as Button).DataContext as Client));
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddEditPage(null));
+        }
+
+        
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible) 
+            {
+                practicaEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridSaloon.ItemsSource = practicaEntities.GetContext().Client.ToList();
+            }
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            var ClientForRemoving = DGridSaloon.SelectedItems.Cast<Client>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {ClientForRemoving.Count()}элементов ?", "Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes) 
+            {
+                try
+                {
+                    practicaEntities.GetContext().Client.RemoveRange(ClientForRemoving);
+                    practicaEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены! ");
+                    DGridSaloon.ItemsSource = practicaEntities.GetContext().Client.ToList();
+
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+    }
+}
